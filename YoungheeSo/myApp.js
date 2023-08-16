215a4a3171d752022a03e33543df3b9099a98179
202216008 <apdlfdldjqtm@gmail.com>
2023-08-16 17:50:45 +0900

    랜덤 출력

---
+++ YoungheeSo/myApp.js
@@ -0,0 +1,22 @@
+
+const Output=[
+    {
+        id:1,
+        text: '양파'
+    },
+    {
+        id:2,
+        text: '허브'
+    }
+
+];
+
+
+
+//출력창에 output 출력하기
+const readOutputs=(Id)=>{
+
+    const $Output = document.querySelector('.read-Output');
+    
+    
+}
