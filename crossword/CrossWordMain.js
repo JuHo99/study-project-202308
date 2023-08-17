130c8385e6ee3a712a59f650f48374a14d378d6a
JoHo99 <ghdi926@naver.com>
2023-08-17 13:50:28 +0900

    0817중간

---
+++ crossword/CrossWordMain.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.gamebox');
+
+
+// ========================================================
+const $menuber = document.querySelector('.lnr-menu');
+const menuberClickHandler = 
+
+$menuber.addEventListener('click',)
+
+// ========================================================
+for (let i = 0; i < 130; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+    
+}
