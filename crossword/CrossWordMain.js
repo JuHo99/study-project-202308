8bfc14e7efb9d277296934c2037c4efe2d039494
JoHo99 <ghdi926@naver.com>
2023-08-17 15:18:33 +0900

    메뉴바 설정 완료

--- crossword/CrossWordMain.js
+++ crossword/CrossWordMain.js
@@ -3,14 +3,25 @@ const container = document.querySelector('.gamebox');
 
 // ========================================================
 const $menuber = document.querySelector('.lnr-menu');
-const menuberClickHandler = 
-
-$menuber.addEventListener('click',)
+const $menu = document.querySelector('.menu')
+const $backdrop = document.getElementById('backdrop')
+const menuberClickHandler = e => {
+    $menu.classList.remove('hide');
+    $backdrop.classList.add('visible')
+}
+const backdropHandler = e => {
+    if (!$menu.classList.contains('hide')) {
+        $menu.classList.add('hide');
+        $backdrop.classList.remove('visible')
+    }
+}
+$backdrop.addEventListener("click", backdropHandler);
+$menuber.addEventListener('click', menuberClickHandler)
 
 // ========================================================
 for (let i = 0; i < 130; i++) {
     const div = document.createElement("div");
     div.classList.add("box");
     container.appendChild(div);
-    
+
 }
