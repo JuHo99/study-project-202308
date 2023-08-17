650dc7f4cece1a431a7b3dd0e23fd71311e9ed51
YoungHeeSo <apdlfdldjqtm@gmail.com>
2023-08-18 01:48:47 +0900

    text 타일 생성

---
+++ YoungheeSo/gamePlay/gamePlay.js
@@ -0,0 +1,16 @@
+
+
+
+const ouputArray = ['출력','사랑','싹', '설산', '예수', '물방아 꽃', '사막', '끓는 열매', '얼음', '봄바람'];   
+
+//ouputArray의 배열의 길이만큼 li 만들기
+const $textbox = document.getElementById('textBox');
+const $newLi = document.createElement('li');
+
+$newLi.textContent='list_6';
+$newLi.setAttribute('id','banana');
+$textbox.appendChild($newLi);
+
+
+
+
