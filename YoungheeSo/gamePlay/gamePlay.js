4069659406692ced89e0e76022bca5f328781934
202216008 <apdlfdldjqtm@gmail.com>
2023-08-18 13:40:05 +0900

    단어 배열 textbx타일에 출력시키기

--- YoungheeSo/gamePlay/gamePlay.js
+++ YoungheeSo/gamePlay/gamePlay.js
@@ -1,15 +1,24 @@
 
 
 
-const ouputArray = ['출력','사랑','싹', '설산', '예수', '물방아 꽃', '사막', '끓는 열매', '얼음', '봄바람'];   
+const outputArray = ['출력','사랑','싹', '설산', '예수', '물방아 꽃', '사막', '끓는 열매', '얼음', '봄바람','마나님', '트럼프'];   
 
 //ouputArray의 배열의 길이만큼 li 만들기
-const $textbox = document.getElementById('textBox');
-const $newLi = document.createElement('li');
+outputArray.forEach(text => {
+    const $textbox = document.getElementById('textBox');
+    const $newLi = document.createElement('li');
 
-$newLi.textContent='list_6';
-$newLi.setAttribute('id','banana');
-$textbox.appendChild($newLi);
+    $newLi.textContent=text;
+    $textbox.appendChild($newLi);
+    
+    console.log(text);
+});
+
+//user input에 text를 쳐서 outputArray배열의 문자를 치면 성공, 없다면 실패
+const userInputText={
+    
+}
+
 
 
 
