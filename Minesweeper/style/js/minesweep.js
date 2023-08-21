53e1c68052aa0f661f610290a4bf3fb7226b26e2
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-08-21 16:02:22 +0900

    파일 변경 및 지뢰 심기&클릭시 열림

--- Minesweeper/style/js/minesweep.js
+++ Minesweeper/style/js/minesweep.js
@@ -8,7 +8,12 @@ const createMineTable = () => {
     $mineTagble.appendChild($tableTr);
     for (let j = 0; j < 10; j++) {
       const $tableTd = document.createElement('td');
+      const $tdBtn = document.createElement('button');
+
+      $tableTd.classList.add('cell');
+
       $tableTr.appendChild($tableTd);
+      $tableTd.appendChild($tdBtn);
     }
   }
 };
@@ -17,34 +22,65 @@ createMineTable();
 //폭탄 위치를 담을 배열
 const sweep = [];
 
-const tableClickHandler = (e) => {
-  console.log(e.target);
+const selectCell = (e) => {
+  const btncell = e.target; //button
+  const clickTd = btncell.parentNode;
 
   //tr 전체를 배열로 변환
-  const $tableTd = $mineTagble.querySelectorAll('tr');
-  const trList = Array.from($tableTd);
+  const trList = Array.from($mineTagble.querySelectorAll('tr'));
 
-  //클릭된 tr에서 td가 몇번째 있는지 확인
-  const tdTr = e.target.closest('.rows');
+  //클릭된 td에서 tr가 몇번째 있는지 확인
+  const tdTr = clickTd.closest('.rows');
   const tdTrList = Array.from(tdTr.children);
 
   //tr과 td 인덱스 뽑아옴
   const oneIndex = trList.indexOf(tdTr);
-  const twoIndex = tdTrList.indexOf(e.target);
-  console.log(oneIndex);
-  console.log(twoIndex);
+  const twoIndex = tdTrList.indexOf(clickTd);
+
+  //클릭한 td 가져옴
+  const clicktable = trList[oneIndex].children[twoIndex];
+  //배경화면 투명으로 변경
+
+  clicktable.classList.add('transparency');
+  clicktable.removeChild(btncell);
+
+  return [oneIndex, twoIndex];
+};
+
+const tableClickHandler = (e) => {
+  const btncell = e.target; //button
+  const clickTd = btncell.parentNode;
+
+  //tr 전체를 배열로 변환
+  const trList = Array.from($mineTagble.querySelectorAll('tr'));
+
+  //클릭된 td에서 tr가 몇번째 있는지 확인
+  const tdTr = clickTd.closest('.rows');
+  const tdTrList = Array.from(tdTr.children);
+
+  //tr과 td 인덱스 뽑아옴
+  const oneIndex = trList.indexOf(tdTr);
+  const twoIndex = tdTrList.indexOf(clickTd);
+
+  //클릭한 td 가져옴
+  const clicktable = trList[oneIndex].children[twoIndex];
+  //배경화면 투명으로 변경
+
+  clicktable.classList.add('transparency');
+  clicktable.removeChild(btncell);
+
   //폭탄 10개를 심을 인덱스 생성
   while (sweep.length !== 10) {
     const one = Math.floor(Math.random() * 10);
     const two = Math.floor(Math.random() * 10);
 
-    //초기 클릭과 one,two가 이미 있는지 확인
+    //초기 클릭위치와 one,two가 이미 있는지 확인
     let isDuplicate = false;
+    if (oneIndex === one && twoIndex === two) {
+      isDuplicate = true;
+    }
     for (const entry of sweep) {
-      if (
-        (entry.one === one && entry.two === two) ||
-        (oneIndex === one && twoIndex === two)
-      ) {
+      if (entry.one === one && entry.two === two) {
         isDuplicate = true;
         break;
       }
@@ -59,8 +95,18 @@ const tableClickHandler = (e) => {
   }
   console.log(sweep);
   //textContent로 넣기
-  sweep.map((swp) => (trList[swp.one].children[swp.two].textContent = 'B'));
+  sweep.map((swp) => {
+    trList[swp.one].children[swp.two].setAttribute('boom', 'B');
+    trList[swp.one].children[swp.two].appendChild(
+      document.createElement('div')
+    );
+  });
   console.log(sweep);
+  $mineTagble.addEventListener('click', deleteButtonHandler);
+};
+
+const deleteButtonHandler = (e) => {
+  selectCell(e);
 };
 
 $mineTagble.addEventListener('click', tableClickHandler, { once: true });
