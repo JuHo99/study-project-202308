b9e333023c2c4dea1c94c622b895fbadb97e5f54
Seonjin <hawooks112@gmail.com>
2023-08-22 19:35:27 +0900

    지뢰찾기 - 난이도에 따라 셀크기 변화

--- Minesweeper/style/js/init_minesweep.js
+++ Minesweeper/style/js/init_minesweep.js
@@ -4,13 +4,18 @@ const $mineTagble = document.getElementById('mineTable');
 //모달
 const $deathModal = document.querySelector('.death_modal');
 
+//레벨 버튼 
+const $easyBtn = document.querySelector('.easy');
+const $nomalBtn = document.querySelector('.nomal');
+const $hardBtn = document.querySelector('.hard');
 //초급 : 9x9/10 , 중급:16x16/40 , 고급:20x16/68
-const ROW = 9;
-const COL = 9;
-const MINE = 10;
+let ROW = 9;
+let COL = 9;
+let MINE = 10;
 
 //폭탄 위치를 담을 배열
 const sweep = [];
+let mineIsValid = false;
 
 //게임 테이블 생성
 const createMineTable = () => {
@@ -32,6 +37,13 @@ const createMineTable = () => {
     }
   }
 };
+
+//테이블 지웠다가 다시 생성 
+const reCreateTable = () =>{
+  $mineTagble.textContent='';
+  createMineTable();
+}
+
 
 // 선택한 셀 찾아오기 =================
 const selectCell = (e) => {
@@ -102,72 +114,79 @@ const tableClickHandler = (e) => {
 
   const { oneIndex, twoIndex, trList } = selectCell(e);
 
+  const setMineAndNum = ()=> {
   //폭탄 10개를 심을 인덱스 생성
-  while (sweep.length !== MINE) {
-    const one = Math.floor(Math.random() * ROW);
-    const two = Math.floor(Math.random() * COL);
+    while (sweep.length !== MINE) {
+      const one = Math.floor(Math.random() * ROW);
+      const two = Math.floor(Math.random() * COL);
 
-    //초기 클릭위치와 one,two가 이미 있는지 확인
-    let isDuplicate = false;
-    if (oneIndex === one && twoIndex === two) {
-      isDuplicate = true;
-    }
-    for (const entry of sweep) {
-      if (entry.one === one && entry.two === two) {
+      //초기 클릭위치와 one,two가 이미 있는지 확인
+      let isDuplicate = false;
+      if (oneIndex === one && twoIndex === two) {
         isDuplicate = true;
-        break;
       }
-    }
-    //false일때 실행
-    if (!isDuplicate) {
-      sweep.push({
-        one: one,
-        two: two,
-      });
-    }
-  }
-  console.log(sweep);
-  //지뢰 넣기
-  for (const swp of sweep) {
-    const { one: trIndex, two: tdIndex } = swp;
-    const tdCell = trList[trIndex].children[tdIndex];
-
-    tdCell.classList.add('boom');
-    tdCell.appendChild(document.createElement('div'));
-  }
-
-  //지뢰 주변에 숫자 넣기
-  for (const swp of sweep) {
-    const { one: trIndex, two: tdIndex } = swp;
-    for (
-      let numTrIndex = trIndex - 1;
-      numTrIndex <= trIndex + 1;
-      numTrIndex++
-    ) {
-      if (numTrIndex === ROW || numTrIndex < 0) {
-        continue;
-      }
-      for (
-        let numTdIndex = tdIndex - 1;
-        numTdIndex <= tdIndex + 1;
-        numTdIndex++
-      ) {
-        const tdTag = trList[numTrIndex].children[numTdIndex];
-        if (
-          numTdIndex === COL ||
-          numTdIndex < 0 ||
-          tdTag.classList.contains('boom') ||
-          (numTdIndex === tdIndex && numTrIndex === trIndex)
-        ) {
-          continue;
-        } else {
-          const $pTag = tdTag.querySelector('.mineNum');
-          $pTag.textContent = +$pTag.textContent + 1;
-          $pTag.setAttribute('Num', '1');
+      for (const entry of sweep) {
+        if (entry.one === one && entry.two === two) {
+          isDuplicate = true;
+          break;
         }
       }
+      //false일때 실행
+      if (!isDuplicate) {
+        sweep.push({
+          one: one,
+          two: two,
+        });
+      }
     }
+    console.log(sweep);
+    //지뢰 넣기
+    for (const swp of sweep) {
+      const { one: trIndex, two: tdIndex } = swp;
+      const tdCell = trList[trIndex].children[tdIndex];
+
+      tdCell.classList.add('boom');
+      tdCell.appendChild(document.createElement('div'));
+    }
+
+    //지뢰 주변에 숫자 넣기
+    for (const swp of sweep) {
+      const { one: trIndex, two: tdIndex } = swp;
+      for (
+        let numTrIndex = trIndex - 1;
+        numTrIndex <= trIndex + 1;
+        numTrIndex++
+      ) {
+        if (numTrIndex === ROW || numTrIndex < 0) {
+          continue;
+        }
+        for (
+          let numTdIndex = tdIndex - 1;
+          numTdIndex <= tdIndex + 1;
+          numTdIndex++
+        ) {
+          const tdTag = trList[numTrIndex].children[numTdIndex];
+          if (
+            numTdIndex === COL ||
+            numTdIndex < 0 ||
+            tdTag.classList.contains('boom') ||
+            (numTdIndex === tdIndex && numTrIndex === trIndex)
+          ) {
+            continue;
+          } else {
+            const $pTag = tdTag.querySelector('.mineNum');
+            $pTag.textContent = +$pTag.textContent + 1;
+            $pTag.setAttribute('Num', '1');
+          }
+        }
+      }
+    } 
   }
+  if(!mineIsValid){
+    setMineAndNum();
+    mineIsValid = true;
+  }
+  
 
   //빈칸을 클릭했을때 자동으로 열리게 하기
   const openAround = (trIndex, tdIndex) => {
@@ -216,7 +235,29 @@ const deleteButtonHandler = (e) => {
   }
 };
 
-$mineTagble.addEventListener('click', tableClickHandler, { once: true });
+$mineTagble.addEventListener('click', tableClickHandler);
 
 // - 화면 렌더링 -
+$easyBtn.addEventListener('click',()=>{
+  ROW = 9
+  COL = 9
+  MINE = 10
+  reCreateTable();
+  mineIsValid = false;
+})
+$nomalBtn.addEventListener('click',()=>{
+  ROW = 16
+  COL = 16
+  MINE = 40
+  reCreateTable();
+  mineIsValid = false;
+})
+$hardBtn.addEventListener('click',()=>{
+  ROW = 20
+  COL = 16
+  MINE = 68
+  reCreateTable();
+  mineIsValid = false;
+})
+
 createMineTable();
