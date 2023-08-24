ba824b821aaf3cb614bb3fc670623934414ffb33
smg0218 <128454773+smg0218@users.noreply.github.com>
2023-08-24 14:30:28 +0900

    Revert "Merge pull request #5 from JuHo99/smg"
    This reverts commit f41032b1c2281686e6366ec89841d2e01a195124.

--- Minesweeper/style/js/main.js
+++ Minesweeper/style/js/main.js
@@ -3,14 +3,14 @@ const $menu = document.querySelector('.menu');
 const $backdrop = document.getElementById('backdrop');
 
 const menuberClickHandler = (e) => {
-    $menu.classList.remove('hide');
-    $backdrop.classList.add('visible');
+  $menu.classList.remove('hide');
+  $backdrop.classList.add('visible');
 };
 const backdropHandler = (e) => {
-    if (!$menu.classList.contains('hide')) {
-        $menu.classList.add('hide');
-        $backdrop.classList.remove('visible');
-    }
+  if (!$menu.classList.contains('hide')) {
+    $menu.classList.add('hide');
+    $backdrop.classList.remove('visible');
+  }
 };
 $backdrop.addEventListener('click', backdropHandler);
 $menuber.addEventListener('click', menuberClickHandler);
